162c04676efd6ba3f7dff41a08f1c55b6357d899
Pumrapee <110712445+Pumrapee@users.noreply.github.com>
2024-04-21 19:51:30 +0700

    base project

---
+++ kanbanapi/src/main/java/sit/int221/kanbanapi/KanbanapiApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.kanbanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanapiApplication.class, args);
+    }
+
+}
---
+++ kanbanapi/src/test/java/sit/int221/kanbanapi/KanbanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.kanbanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
