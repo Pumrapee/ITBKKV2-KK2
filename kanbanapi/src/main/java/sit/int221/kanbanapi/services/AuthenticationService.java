59dfa3dbcce4f7c2fe2fde8042f4aaf21efdefcb
Pumrapee <110712445+Pumrapee@users.noreply.github.com>
2024-08-18 00:29:43 +0700

    validate backend

--- kanbanapi/src/main/java/sit/int221/kanbanapi/services/AuthenticationService.java
+++ kanbanapi/src/main/java/sit/int221/kanbanapi/services/AuthenticationService.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import sit.int221.kanbanapi.databases.userdb.entities.User;
 import sit.int221.kanbanapi.databases.userdb.repositories.UserRepository;
+import sit.int221.kanbanapi.exceptions.AuthenticationFailed;
 
 
 @Service
@@ -16,7 +17,7 @@ public class AuthenticationService {
     PasswordEncoder passwordEncoder;
 
     public boolean authenticate(String username, String password) {
-        User user = userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new AuthenticationFailed("username or password incorrect"));
         return passwordEncoder.matches(password, user.getPassword());
     }
 }
